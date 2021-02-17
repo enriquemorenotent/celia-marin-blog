828ec1e3e45157a04898fc181e6d16a537905595
Enrique Moreno <enriquemorenotent@gmail.com>
2021-02-17 08:47:39 +0100

    Update types

--- functions/set-custom-post-types.php
+++ functions/set-custom-post-types.php
@@ -2,17 +2,18 @@
 
 function set_custom_post_types()
 {
-    // register_post_type("works", [
-    //     "labels" => [
-    //         "name" => "Trabajos",
-    //         "singular_name" => "Trabajo",
-    //         "menu_name" => "Trabajos"
-    //     ],
-    //     "public" => true,
-    //     "capability_type" => "post",
-    //     "has_archive" => true,
-    //     "supports" => ["title, editor"]
-    // ]);
+    register_post_type("work", [
+        "labels" => [
+            "name" => "Trabajos",
+            "singular_name" => "Trabajo",
+            "menu_name" => "Trabajos"
+        ],
+        "public" => true,
+        "capability_type" => "post",
+        "has_archive" => true,
+        "show_in_rest" => true,
+        'supports' => ['title', 'editor',  'excerpt', 'thumbnail'],
+    ]);
 }
 
 add_action("init", "set_custom_post_types");
