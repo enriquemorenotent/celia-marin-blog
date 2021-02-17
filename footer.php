dff2a58b9fe477f5537867ae4e4945ddefcec0c1
Enrique Moreno <enriquemorenotent@gmail.com>
2021-02-17 07:36:56 +0100

    JS added

--- footer.php
+++ footer.php
@@ -9,14 +9,15 @@
 
 			</div>
 
+
 		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.scrollex.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/browser.min.js"></script>
-			<script src="assets/js/breakpoints.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
+			<script src="<?= get_template_directory_uri() ?>/assets/js/jquery.min.js"></script>
+			<script src="<?= get_template_directory_uri() ?>/assets/js/jquery.scrollex.min.js"></script>
+			<script src="<?= get_template_directory_uri() ?>/assets/js/jquery.scrolly.min.js"></script>
+			<script src="<?= get_template_directory_uri() ?>/assets/js/browser.min.js"></script>
+			<script src="<?= get_template_directory_uri() ?>/assets/js/breakpoints.min.js"></script>
+			<script src="<?= get_template_directory_uri() ?>/assets/js/util.js"></script>
+			<script src="<?= get_template_directory_uri() ?>/assets/js/main.js"></script>
 
 	</body>
 </html>
--- header.php
+++ header.php
@@ -7,3 +7,29 @@
         <?php wp_head() ?>
     </head>
     <body <?php body_class("is-preload") ?>>
+        <!-- Header -->
+        <section id="header">
+            <header>
+                <span class="image avatar"><img src="images/avatar.jpg" alt="" /></span>
+                <h1 id="logo"><a href="#">Celia Marín</a></h1>
+                <p>Locución y Comunicación Creativa</p>
+            </header>
+            <nav id="nav">
+                <ul>
+                    <li><a href="#about-me" class="active">Sobre mi</a></li>
+                    <li><a href="#works">Mis trabajos</a></li>
+                    <li><a href="#blog">Blog</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+            <footer>
+                <ul class="icons">
+                    <li><a href="https://www.linkedin.com/in/celia-mar%C3%ADn-benito/" class="icon brands fa-linkedin"><span class="label">Linkedin</span></a></li>
+                    <li><a href="https://www.facebook.com/celia.marin.benito" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+                    <li><a href="https://www.instagram.com/celiamarin/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+                    <li><a href="https://twitter.com/celiamarin" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+                    <li><a href="mailto:celiamarben@gmail.com" class="icon solid fa-envelope" target="_blank"><span class="label">Email</span></a></li>
+                </ul>
+            </footer>
+        </section>
+
