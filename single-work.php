89884ed8fe4c487e9ea01471f06053a3e8b297c8
Enrique Moreno <enriquemorenotent@gmail.com>
2021-02-17 10:04:51 +0100

    Added single for work

---
+++ single-work.php
@@ -0,0 +1,30 @@
+asd<!-- single.php -->
+<?php get_header() ?>
+    <!-- Wrapper -->
+        <div id="wrapper">
+            <div id="main">
+            <section id="about-me">
+                <div class="image main" data-position="center">
+                    <a href="/"><?= the_custom_logo(); ?></a>
+                </div>
+            </section>
+
+            <section>
+                <div class="container">
+                    <?php if ( have_posts() ) : ?>
+                        <?php while ( have_posts() ) : ?>
+                            <?php the_post(); ?>
+
+                            <header class="major">
+                                <h2><?php the_title(); ?></h2>
+                            </header>
+
+                            <?php the_content(); ?>
+                        <?php endwhile; ?>
+                    <?php endif; ?>
+
+                </div>
+            </section>
+        </div>
+
+<?php get_footer() ?>
