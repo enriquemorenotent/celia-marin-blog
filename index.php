5dacc9cf9021e96a2fd3d95cc2bd1855eb60b9cf
Enrique Moreno <enriquemorenotent@gmail.com>
2021-02-18 17:43:27 +0100

    added linked blogs

--- index.php
+++ index.php
@@ -12,12 +12,13 @@
                                 <a href="/"><?= the_custom_logo(); ?></a>
                             </div>
                             <div class="container">
+
                                 <?php if ( have_posts() ) : ?>
                                     <?php while ( have_posts() ) : ?>
                                         <?php the_post(); ?>
 
                                         <header class="major">
-                                            <h2><?php the_title(); ?></h2>
+                                            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                                         </header>
 
                                     <?php endwhile; ?>
