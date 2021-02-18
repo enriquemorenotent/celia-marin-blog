0af984c6c66aa8c73a901214870499c387532a20
Enrique Moreno <enriquemorenotent@gmail.com>
2021-02-18 19:36:40 +0100

    404

---
+++ 404.php
@@ -0,0 +1,22 @@
+<!-- index.php -->
+<?php get_header() ?>
+    <!-- Wrapper -->
+        <div id="wrapper">
+
+            <!-- Main -->
+                <div id="main">
+
+                    <!-- One -->
+                        <section>
+                            <div class="image main" data-position="center">
+                                <a href="/"><?= the_custom_logo(); ?></a>
+                            </div>
+                            <div class="container">
+                                <header class="major">
+                                    <h2>404 - Page not found! :(</h2>
+                                </header>
+                            </div>
+                        </section>
+                </div>
+
+<?php get_footer() ?>
